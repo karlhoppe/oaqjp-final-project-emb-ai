2152cda96003dcf5ecb0977db8a4090247c64c81
karlhoppe <karlhoppe@users.noreply.github.com>
2025-04-13 23:04:17 -0400

    final

--- server.py
+++ server.py
@@ -1,3 +1,10 @@
+"""Module: Emotion Detection
+Author: Karl Hoppe
+Date: April 13, 2025
+Version: 1.0
+"""
+
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +12,9 @@ app=Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """a function to analyze emotion
+    arg is a phrase
+    """
     text_to_analyze = request.args.get('textToAnalyze')
 
     response=emotion_detector(text_to_analyze)
@@ -17,14 +27,21 @@ def sent_analyzer():
     sadness=response['sadness']
 
     if dominant_emotion == 'none':
-        return "Invalid text!  Please try again!"
+        myresponse= "Invalid text!  Please try again!"
     else:
-        return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {}, 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant_emotion)
-    
+        myresponse1=f"For the given statement, the system response is 'anger': {anger}, "
+        myresponse2=f"'disgust': {disgust},'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}. "
+        myresponse3=f"The dominant emotion is {dominant_emotion}."
+        myresponse = myresponse1+myresponse2+myresponse3
+
+    return myresponse
 
 @app.route("/")
 def render_index_page():
-    return render_template('index.html')  
+    """a function to show index page
+    I like this here
+    """
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
